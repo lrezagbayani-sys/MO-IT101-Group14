eee0919b9a8d945f0b719ff0327b046f537f9b96
lrezagbayani-sys <lr.ezagbayani@mmdc.mcl.edu.ph>
2026-02-19 16:37:12 +0800

    MotorPH.java (1.1)
    >Header + Another testing class
    
    I'll do the computations

--- motorph/MotorPH.java
+++ motorph/MotorPH.java
@@ -2,16 +2,52 @@ package com.mycompany.motorph;
 
 public class MotorPH {
     public static void main(String[] args) {
+        
+        System.out.printf(
+            "%-12s %-20s %-20s %-20s %-20s %-20s %-20s %-20s %-20s %-20s%n",
+            "Employee #", 
+            "Name", 
+            "Phone", 
+            "SSS #", 
+            "PhilHealth #",
+            "TIN #", 
+            "Pag-Ibig #", 
+            "Status", 
+            "Role", 
+            "Supervisor"
+        );
+        
         Employee emp1 = new Employee(
-            101,               
-            "John",
-            "Doe",
+            1,               
+            "Ethan",
+            "Ziv",
+            "09999999999",
+            "44-4506057-3",
+            "820126853951",
+            "442-605-657-000",
+            "691295330870",
+            true,
             Roles.CUSTOMER_SERVICE,
-            "09171234567",    
-            true
+            "bossName"
         );
 
         emp1.displayInfo();
+        
+        Employee emp2 = new Employee(
+            1,               
+            "First",
+            "Person",
+            "091111111111",
+            "44-4506057-2",
+            "820126853951",
+            "442-605-657-000",
+            "691295330870",
+            true,
+            Roles.CUSTOMER_SERVICE,
+            "bossName"
+        );
+
+        emp2.displayInfo();
     }
 }
 

29b601bc7f02ddbc6026e9bb4a9e028c06c5f543
lrezagbayani-sys <lr.ezagbayani@mmdc.mcl.edu.ph>
2026-02-24 13:45:03 +0800

    Undo.java

--- motorph/MotorPH.java
+++ motorph/MotorPH.java
@@ -1,40 +1,5 @@
 package com.mycompany.motorph;
 
-package com.mycompany.motorph;
-
-import java.io.FileInputStream;
-import org.apache.poi.ss.usermodel.*; //this import let creates classes for POI
-import org.apache.poi.xssf.usermodel.XSSFWorkbook; //this import let POI use its function for excel
-
-public class MotorPH {
-    public static void main(String[] args) {
-        System.out.printf("%-12s %-20s %-20s%n", //headers for category, add new headers for new primitives (PhilHealth, TIN, Pag-Ibig etc. etc.)
-                "Employee #", 
-                "First Name", 
-                "Last Name");
-
-        try (FileInputStream fis = new FileInputStream("employeedata.xlsx"); Workbook workbook = new XSSFWorkbook(fis)) { //Opens the Excel for our file
-
-            for (Row row : workbook.getSheetAt(0)) {
-                if (row.getRowNum() == 0) continue; //skips the header for category
-
-                Cell c0 = row.getCell(0); //takes employee number from column A (For POI, its 0 and B=1, C=2, so on)
-                Cell c1 = row.getCell(1); //takes first name from column b
-                Cell c2 = row.getCell(2); //takes last name from column c
-
-                if (c0 == null || c1 == null || c2 == null) continue; //skips if function didnt read anything
-
-                System.out.printf("%-12d %-20s %-20s%n", //prints employee number using .get_CellValues functions
-                        (int) c0.getNumericCellValue(), //this needs a integer cast
-                        c1.getStringCellValue(),
-                        c2.getStringCellValue());
-            }
-        } catch (Exception e) { e.printStackTrace(); //
-        }
-    }
-}
-
-/* //OLD VERSION
 public class MotorPH {
     public static void main(String[] args) {
         
@@ -81,8 +46,5 @@ public class MotorPH {
             Roles.CUSTOMER_SERVICE,
             "bossName"
         );
-
-        emp2.displayInfo();
     }
 }
-*/

439d7651cce86d45955cb2c80caf51329b27a328
lrezagbayani-sys <lr.ezagbayani@mmdc.mcl.edu.ph>
2026-02-26 12:08:52 +0800

    Update MotorPH.java

--- motorph/MotorPH.java
+++ motorph/MotorPH.java
@@ -2,49 +2,24 @@ package com.mycompany.motorph;
 
 public class MotorPH {
     public static void main(String[] args) {
-        
-        System.out.printf(
-            "%-12s %-20s %-20s %-20s %-20s %-20s %-20s %-20s %-20s %-20s%n",
-            "Employee #", 
-            "Name", 
-            "Phone", 
-            "SSS #", 
-            "PhilHealth #",
-            "TIN #", 
-            "Pag-Ibig #", 
-            "Status", 
-            "Role", 
-            "Supervisor"
-        );
-        
-        Employee emp1 = new Employee(
-            1,               
-            "Ethan",
-            "Ziv",
-            "09999999999",
-            "44-4506057-3",
-            "820126853951",
-            "442-605-657-000",
-            "691295330870",
-            true,
-            Roles.CUSTOMER_SERVICE,
-            "bossName"
-        );
+
+        Employee emp1 = new Employee();
+
+        emp1.employeeNum = 10000;
+        emp1.firstName = "Ethan";
+        emp1.lastName = "Agbayani";
+        emp1.birthMonth = 1;
+        emp1.birthDay = 1;
+        emp1.birthYear = 1;
+        emp1.phoneNum = "placeHolder"; 
+        emp1.sssNum = "placeHolder"; 
+        emp1.phNum = "placeHolder"; 
+        emp1.tinNum = "placeHolder";
+        emp1.ibigNum = "placeHolder";
+        emp1.isRegular = true;
+        emp1.roles = "Customer Service";
+        emp1.supervisor = "placeHolder";
 
         emp1.displayInfo();
-        
-        Employee emp2 = new Employee(
-            1,               
-            "First",
-            "Person",
-            "091111111111",
-            "44-4506057-2",
-            "820126853951",
-            "442-605-657-000",
-            "691295330870",
-            true,
-            Roles.CUSTOMER_SERVICE,
-            "bossName"
-        );
     }
 }

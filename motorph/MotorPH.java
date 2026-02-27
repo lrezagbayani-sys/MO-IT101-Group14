46a14f10a75d3317f3735d5b0de8fd967a5b8b38
lrezagbayani-sys <lr.ezagbayani@mmdc.mcl.edu.ph>
2026-02-27 08:05:31 +0800

    Update MotorPH.java

--- motorph/MotorPH.java
+++ motorph/MotorPH.java
@@ -1,6 +1,7 @@
-package com.mycompany.motorph;
-
 public class MotorPH {
+    
+    //Create employee test object
+    
     public static void main(String[] args) {
 
         Employee emp1 = new Employee();
@@ -21,5 +22,40 @@ public class MotorPH {
         emp1.supervisor = "placeHolder";
 
         emp1.displayInfo();
+    }
+}
+
+    //Declares primitive/data type for each variable within the employee class
+
+    class Employee {
+        int employeeNum;
+        String firstName;
+        String lastName;
+        int birthDay;
+        int birthMonth;
+        int birthYear;
+        String phoneNum;
+        String sssNum;
+        String phNum;
+        String tinNum;
+        String ibigNum;
+        boolean isRegular;
+        String roles;
+        String supervisor;
+        
+    //Presents data in a format/structure using println 
+
+    public void displayInfo() {
+        System.out.println("Employee #: " + employeeNum);
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("Birthdate: " + birthMonth + "/" + birthDay + "/" + birthYear);
+        System.out.println("Phone: " + phoneNum);
+        System.out.println("SSS: " + sssNum);
+        System.out.println("PhilHealth: " + phNum);
+        System.out.println("TIN: " + tinNum);
+        System.out.println("Pag-IBIG: " + ibigNum);
+        System.out.println("Status: " + (isRegular ? "Regular" : "Probationary"));
+        System.out.println("Role: " + roles);
+        System.out.println("Supervisor: " + supervisor);
     }
 }
